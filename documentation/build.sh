aa52e2f23005c395c87d6dae5d9e1b98170d4813
Harald Braun <braunharald@tigris.org>
2007-04-28 06:44:37 +0000

    First Check-In
    git-svn-id: http://argouml-de.tigris.org/svn/argouml-de/trunk@103 52cefb5e-b31d-0410-9e77-a1dba70b380d

---
+++ documentation/build.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+#
+
+# $Id: build.sh 59 2007-01-26 08:48:50Z linus $
+
+#
+# This script assumes the ant that comes with argouml.
+# If you have ant installed elsewhere (required if you don't have argouml
+# checked out, use that instead of running build.sh
+
+ANT_HOME=../argouml/tools/apache-ant-1.6.5
+
+echo ANT_HOME is: $ANT_HOME
+echo
+echo Starting Ant...
+echo
+
+$ANT_HOME/bin/ant $*
+
+exit
